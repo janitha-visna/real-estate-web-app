d25a47188870eae8a191fa81a15268883575f43d
janitha-visna <visnajanitha@gmail.com>
2024-06-11 20:39:03 +0530

    implementing react leaflet map

---
+++ src/components/pin/Pin.jsx
@@ -0,0 +1,24 @@
+import { Popup, Marker } from "react-leaflet";
+import './pin.scss'
+import { Link } from "react-router-dom";
+
+
+
+function Pin({item}) {
+  return (
+    <Marker position={[item.latitude, item.longitude]}>
+      <Popup>
+        <div className="popupContainer">
+          <img src={item.images} alt="" />
+          <div className="textContainer">
+            <Link to={`/${item.id}`}>{item.title}</Link>
+            <span >{item.bedroom}</span>
+            <b>${item.price}</b>
+          </div>
+        </div>
+      </Popup>
+    </Marker>
+  );
+}
+
+export default Pin
