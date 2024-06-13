d36a07b330765d539e467b7d438fe2d80957a039
janitha-visna <visnajanitha@gmail.com>
2024-06-14 01:26:30 +0530

    messenger chat window

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./chat.scss";
 
 function Chat() {
+    const [chat,setChat] = useState(true)
   return (
     <div className="chat">
       <div className="messages">
@@ -38,7 +40,46 @@ function Chat() {
           <p>Lorem ipsum dolor sit amet consectetur</p>
         </div>
       </div>
-      <div className="chatBox"></div>
+      {chat && (
+        <div className="chatBox">
+          <div className="top">
+            <div className="user">
+              <img
+                src="https://images.pexels.com/photos/1428348/pexels-photo-1428348.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt=""
+              />
+              jhon doe
+            </div>
+            <span className="close" onClick={() => setChat(null)}>x</span>
+          </div>
+          <div className="centre">
+            <div className="chatMessage own">
+              <p>Lorem ipsum dolor sit </p>
+              <span>1 hor ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Lorem ipsum dolor sit </p>
+              <span>1 hor ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Lorem ipsum dolor sit </p>
+              <span>1 hor ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Lorem ipsum dolor sit </p>
+              <span>1 hor ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Lorem ipsum dolor sit </p>
+              <span>1 hor ago</span>
+            </div>
+          </div>
+          <div className="bottom">
+            <textarea></textarea>
+            <button>send</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
