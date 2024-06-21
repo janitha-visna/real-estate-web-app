dc555383a159187928191eecde869ace27f57d38
janitha-visna <visnajanitha@gmail.com>
2024-06-21 12:40:45 +0530

    implment a middleware

--- api/controllers/test.controller.js
+++ api/controllers/test.controller.js
@@ -1,14 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const shouldBeLoggedIn = async (req, res) => {
-  const token = req.cookies.token;
-
-  if (!token) return res.status(401).json({ message: "not authenticated" });
-
-  jwt.verify(token, process.env.JWT_SECRECT_KEY, async (err, payload) => {
-    if (err) return res.status(403).json({ message: "token is not valid" });
-  });
-
+    console.log(req.userId);
   res.status(200).json({ message: "you are authenticated" });
 };
 
