0c21a97c303d4d8611ec92b066c9e55906da4b15
janitha-visna <visnajanitha@gmail.com>
2024-07-02 23:25:59 +0530

    added profileposts route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -5,15 +5,17 @@ import {
   getUsers,
   updateUsers,
   savePost,
+  profilePosts,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
-router.get("/:id", verifyToken, getUser);
+//router.get("/search/:id", verifyToken, getUser);
 router.put("/:id", verifyToken, updateUsers);
 router.delete("/:id", verifyToken, deleteUser);
-router.post("/:save", verifyToken, savePost);
+router.post("/save", verifyToken, savePost);
+router.get("/profilePosts", verifyToken, profilePosts);
 
 export default router;
