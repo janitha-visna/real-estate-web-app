06b7f15eb1df20a1e183121fb924fbd370409104
janitha-visna <visnajanitha@gmail.com>
2024-07-03 21:07:03 +0530

    added chatroute and messageroute

--- api/app.js
+++ api/app.js
@@ -5,6 +5,9 @@ import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
+
 
 const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
@@ -16,6 +19,9 @@ app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/test", testRoute);
 app.use("/api/users", userRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
+
 
 app.listen(8080, () => {
   console.log("server is running!");
